0ddf6d05e97b02f9fe9e8ec23a01910b970ca455
knsugit <knsugit@outlook.com>
2020-01-24 17:25:24 +0800

    Github accessToken

---
+++ src/main/java/life/majiang/community/demo/controller/AuthorizeController.java
@@ -0,0 +1,31 @@
+package life.majiang.community.demo.controller;
+
+import life.majiang.community.demo.dto.AccessTokenDTO;
+import life.majiang.community.demo.dto.GithubUser;
+import life.majiang.community.demo.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("04a67abac3a387ee1b97");
+        accessTokenDTO.setClient_secret("e6b5c7a31b8cba1a266e4c3efad811aa40fcd856");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
